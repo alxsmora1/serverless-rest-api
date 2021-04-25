78ada2b525843fa285b78cf35721815c9a16c0c7
Alexis Mora <alexis.mora1v@gmail.com>
2021-04-25 00:43:14 -0500

    Add the connection to database and method get
    Add the connection string to the database.
    Add the method get, to retrieve the list of the pets from the
    database.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,38 @@
 const functions = require("firebase-functions");
+const express = require('express');
+const mongoose = require('mongoose');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const mongooseConfig = { useNewUrlParser: true };
+mongoose.connect('CONNECTION_STRING', mongooseConfig);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
-});
+const app = express();
+
+const Pets = require('./pets');
+
+const createServer = () => {
+    app.get('/pets', async (req, res) => {
+        //const pet = new Pets({
+        //    nombre: 'Cerdito feliz',
+        //    tipo: 'Cerdo',
+        //    descripcion: 'Esta triste porque su amigo no para de comer',
+        //});
+
+        //pet.save();
+        
+        const result = await Pets.find({}).exec();
+
+        res.send(result);
+    });
+    
+    app.post('/pets', (req, res) => {
+        res.send('creando una mascota');
+    });
+
+    app.get('/pets/:id/daralta', (req, res) => {
+        res.send('dar alta');
+    });
+
+    return app;
+}
+
+exports.api = functions.https.onRequest(createServer());
