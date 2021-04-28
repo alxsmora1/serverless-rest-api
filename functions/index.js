a3f456a563938c776d75d080f5dc7d76cd734099
Alexis Mora <alexis.mora1v@gmail.com>
2021-04-27 20:05:48 -0500

    Complete function and add secrets vars
    Run prettier over the file.
    Add secrets vars troughout firebase config.
    Complete the daralta function to send an id of a pet.

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,48 @@
 const functions = require("firebase-functions");
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const mongooseConfig = { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true, 
+const mongooseConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
-mongoose.connect('CONNECTION_STRING', mongooseConfig);
+
+const {username, password} = functions.config().mongo;
+
+const mongoUri = `mongodb+srv://${username}:${password}@cluster0.wvmrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoUri, mongooseConfig);
 
 const app = express();
 
-const Pets = require('./pets');
+const Pets = require("./pets");
 
 const createServer = () => {
-    app.get('/pets', async (req, res) => {
-        const result = await Pets.find({}).exec();
+  app.use(cors({origin: true}));
+  app.get("/pets", async (req, res) => {
+    const result = await Pets.find({}).exec();
 
-        res.send(result);
-    });
-    
-    app.post('/pets', async (req, res) => {
-        const { body } =  req;
+    res.send(result);
+  });
 
-        const pet = new Pets(body);
-        await pet.save();
-        res.sendStatus(204);
-    });
+  app.post("/pets", async (req, res) => {
+    const {body} = req;
 
-    app.get('/pets/:id/daralta', (req, res) => {
-        res.send('dar alta');
-    });
+    const pet = new Pets(body);
+    await pet.save();
+    res.sendStatus(204);
+  });
 
-    return app;
-}
+  app.get("/pets/:id/daralta", async (req, res) => {
+    const {id} = req.params;
+
+    await Pets.deleteOne({_id: id});
+
+    res.sendStatus(204);
+  });
+
+  return app;
+};
 
 exports.api = functions.https.onRequest(createServer());
